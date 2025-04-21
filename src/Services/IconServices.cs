d66b0cfb59a34b1694cf01aa8e5a5990c9f6784f
FMagalhaess <7filipe093@gmail.com>
2025-04-21 17:03:22 -0300

    Add actual projects

--- src/Services/IconServices.cs
+++ src/Services/IconServices.cs
@@ -5,6 +5,29 @@ public class IconServices{
     static string size = "80";
     List<Icon> IconList = new List<Icon>{
         new Icon{
+            Id = 3,
+            Name = "C#",
+            Url = $"https://img.icons8.com/?size={size}&id=Fycm8TUhWmFU&format=png&color=000000"
+        },
+        new Icon()
+        {
+            Id = 11,
+            Name = ".NET",
+            Url = $"https://img.icons8.com/?size={size}&id=1BC75jFEBED6&format=png&color=000000"
+        },
+        new Icon()
+        {
+            Id = 9,
+            Name = "PHP",
+            Url = $"https://img.icons8.com/?size={size}&id=JybIpZjjXT0F&format=png&color=000000"
+        },
+        new Icon()
+        {
+            Id = 12,
+            Name = "Laravel",
+            Url = $"https://img.icons8.com/?size={size}&id=lRjcvhvtR81o&format=png&color=000000"
+        },
+        new Icon{
             Id = 1,
             Name = "Javascript",
             Url = $"https://img.icons8.com/?size={size}&id=108784&format=png&color=000000"
@@ -13,11 +36,6 @@ public class IconServices{
             Id = 2,
             Name = "Typescript",
             Url = $"https://img.icons8.com/?size={size}&id=uJM6fQYqDaZK&format=png&color=000000"
-        },
-        new Icon{
-            Id = 3,
-            Name = "C#",
-            Url = $"https://img.icons8.com/?size={size}&id=Fycm8TUhWmFU&format=png&color=000000"
         },
         new Icon{
             Id = 4,
@@ -46,33 +64,46 @@ public class IconServices{
         },
         new Icon()
         {
-            Id = 9,
-            Name = "PHP",
-            Url = $"https://img.icons8.com/?size={size}&id=JybIpZjjXT0F&format=png&color=000000"
-        },
-        new Icon()
-        {
             Id = 10,
             Name = "Docker",
             Url = $"https://img.icons8.com/?size={size}&id=22813&format=png&color=000000"
         },
         new Icon()
         {
-            Id = 11,
-            Name = ".NET",
-            Url = $"https://img.icons8.com/?size={size}&id=1BC75jFEBED6&format=png&color=000000"
+            Id = 13,
+            Name = "MySql",
+            Url = $"https://img.icons8.com/?size={size}&id=39855&format=png&color=000000"
         },
         new Icon()
         {
-            Id = 12,
-            Name = "PHP",
-            Url = $"https://img.icons8.com/?size={size}&id=JybIpZjjXT0F&format=png&color=000000"
+            Id = 14,
+            Name = "Bootstrap",
+            Url = $"https://img.icons8.com/?size={size}&id=g9mmSxx3SwAI&format=png&color=000000"
         },
+        new Icon()
+        {
+            Id = 15,
+            Name = "Flutter",
+            Url = $"https://img.icons8.com/?size={size}&id=5pu47piHKg1I&format=png&color=000000"
+        },
+        
     };
     public List<Icon> GetIcons(){
         return IconList;
     }
-    public Icon GetIconByName(string name){
-        return IconList.Find(x => x.Name == name);
+    public Icon GetIconByName(string name)
+    {
+        Icon? icon = IconList.Find(x => x.Name == name);
+        if (icon == null)
+        {
+            icon = new Icon()
+            {
+                Id = 9999999,
+                Name = "not found",
+                Url = $"https://img.icons8.com/?size={size}&id=45967&format=png&color=000000"
+            };
+        }
+
+        return icon;
     }
 }
--- src/Services/ProjetosServices.cs
+++ src/Services/ProjetosServices.cs
@@ -12,25 +12,46 @@ public class ProjetosServices
         "molestie. Nunc cursus est libero, vel ullamcorper purus bibendum at. Donec congue faucibus varius. Orci varius" +
         "natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vestibulum pellentesque" +
         "risus id finibus. Quisque non turpis semper, rhoncus enim nec, venenatis magna.";
+
+    private static readonly string MarketPlaceDescription = "Em cidades pequenas costumamos comprar comida por aplicativos" +
+                                                   " bem como nas capitais, mas eletrodomésticos, itens de construção, itens" +
+                                                   "de manutenção, entre outros são comprados indo diretamente na loja," +
+                                                   " mas isso gera um problema para aqueles que tem menos tempo e/ou " +
+                                                   "disponibilidade para sair de casa. Foi pensando nisso que em" +
+                                                   " dupla com um amigo da mesma  cidade que eu, iniciamos o" +
+                                                   "desenvolvimento do aplicativo ainda sem nome. Um marketplace " +
+                                                   "que visa ser uma versão do mercado livre voltada para cidades " +
+                                                   "menores, onde se compra da mesma cidade onde está o comprador, " +
+                                                   "movimentando assim o mercado local. Utilizamos PHP Laravel para o " +
+                                                   "back-end, para o desenvolvimento mobile optamos por Flutter. Atualmente estamos desenvolvendo as telas em flutter.\n";
+
+    private static readonly string CalculadoraDesc = "Quando eu era dono de uma hamburgueria a coisa que eu mais tinha" +
+                                                     " dificuldade era precificar o meu lanche, conversando" +
+                                                     " com outros amigos donos de lanchonetes, percebi que é um" +
+                                                     " problema que não era apenas meu, com isso decidi desenvolver" +
+                                                     " uma calculadora que ajudaria eles com isso. Esse projeto ainda" +
+                                                     " encontra-se em desenvolvimento, uso C# .NET para o back-end e" +
+                                                     " planejo utilizar Flutter para o front mobile e react para o" +
+                                                     " front WEB";
     List<Projeto> projetos = new List<Projeto> {
             new Projeto {
                 Id = 1,
-                Nome = "Projeto 1",
-                Descricao = loren,
+                Nome = "Marketplace com laravel",
+                Descricao = MarketPlaceDescription,
                 Imagem = "https://i.postimg.cc/MTSWSvGF/Captura-de-tela-de-2024-09-09-10-48-27.png",
-                PrincipalStacks = new List<string> { "Javascript", "Git", "C#" },
+                PrincipalStacks = new List<string> { "Laravel", "Flutter", "Git" },
             },
             new Projeto {
                 Id = 2,
-                Nome = "Projeto 2",
-                Descricao = "Descrição do projeto 2",
+                Nome = "Calculadora de custos para lanchonetes",
+                Descricao = CalculadoraDesc,
                 Imagem = "https://i.postimg.cc/MTSWSvGF/Captura-de-tela-de-2024-09-09-10-48-27.png",
-                PrincipalStacks = new List<string> { "Typescript", "SqlServer", "Python" },
+                PrincipalStacks = new List<string> { "C#", ".NET", "Flutter" },
             },
             new Projeto{
                 Id = 3,
                 Nome = "atque",
-                Descricao = "Landing",
+                Descricao = loren,
                 Imagem = "https://placehold.co/6000/png",
                 PrincipalStacks = new List<string> { "Html", "C#", "Css" },
 
